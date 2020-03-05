97f94abfc439bfc76763f0b7f8f3a53eb9c2ee0b
maroder <looterofflux@gmail.com>
2020-03-05 17:26:38 +0100

    Implementation driver

--- src/main/java/fr/zenity/desbugs/Enum/Device.java
+++ src/main/java/fr/zenity/desbugs/Enum/Device.java
@@ -2,14 +2,29 @@ package fr.zenity.desbugs.Enum;
 
 public enum Device {
 
-    DESKTOP(-1,-1);
+    DESKTOP(-1,-1, true);
 
     private final int width;
     private final int height;
+    private final boolean isRealDevice;
 
-    Device( int width, int height ){
-        this.width  = width;
-        this.height = height;
+
+    Device( int width, int height, boolean isRealDevice ){
+        this.width          = width;
+        this.height         = height;
+        this.isRealDevice   = isRealDevice;
+    }
+
+    public int getWidth( ){
+        return this.width;
+    }
+
+    public int getHeight( ){
+        return this.height;
+    }
+
+    public boolean isRealDevice(){
+        return this.isRealDevice;
     }
 
 }
